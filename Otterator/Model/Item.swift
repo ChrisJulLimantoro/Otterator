3060bdd7af839b58ea0398dbe0c9268c626c565b
bayuuat <bayuuat@gmail.com>
2024-07-12 16:31:41 +0700

    feat: create item model

--- Otterator/Model/Item.swift
+++ Otterator/Model/Item.swift
@@ -9,10 +9,28 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
-    var timestamp: Date
+class Item {
+    var id: UUID
+    var title: String
+    var audio_file: String
+    var datetime: Date
+    var duration: Double
+    var transcript: [WordTranscript]
+    var avg_pitch : Double
+    var avg_volume : Double
+    var avg_pause : Double
+    var avg_pace : Double
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    init(id: UUID, title: String, audio_file: String, datetime: Date, duration: Double, transcript: [WordTranscript], avg_pitch: Double, avg_volume: Double, avg_pause: Double, avg_pace: Double) {
+        self.id = id
+        self.title = title
+        self.audio_file = audio_file
+        self.datetime = datetime
+        self.duration = duration
+        self.transcript = transcript
+        self.avg_pitch = avg_pitch
+        self.avg_volume = avg_volume
+        self.avg_pause = avg_pause
+        self.avg_pace = avg_pace
     }
 }
---
+++ Otterator/Model/WordTranscript.swift
@@ -0,0 +1,38 @@
+//
+//  Word.swift
+//  Otterator
+//
+//  Created by Bayu Aditya Triwibowo on 12/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class WordTranscript {
+    var word : String
+    var timestamp : Double
+    var duration : Double
+    var voice_analysis: VoiceAnalyst
+    var avg_pitch : Double
+    var avg_volume : Double
+    var corrected_word : String
+    var is_pause: Bool
+    
+    init(word: String, timestamp: Double, duration: Double, voice_analysis: VoiceAnalyst, avg_pitch: Double, avg_volume: Double, corrected_word: String, is_pause: Bool) {
+        self.word = word
+        self.timestamp = timestamp
+        self.duration = duration
+        self.voice_analysis = voice_analysis
+        self.avg_pitch = avg_pitch
+        self.avg_volume = avg_volume
+        self.corrected_word = corrected_word
+        self.is_pause = is_pause
+    }
+}
+
+struct VoiceAnalyst {
+    var pitch : [Double]
+    var volume : [Double]
+}
+    
