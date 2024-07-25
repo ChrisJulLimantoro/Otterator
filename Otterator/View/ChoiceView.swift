ed940998355ed932d3c989c5c9618b4cae5431eb
michellealvera <malveralolang@student.ciputra.ac.id>
2024-07-25 13:55:26 +0700

    Add ChoiceView (evaluate and practice) and CustomSegmentedControl. Change the UI of SummaryView, TranscriptView, RecordingView

---
+++ Otterator/View/ChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  ChoiceView.swift
+//  Otterator
+//
+//  Created by Michelle Alvera Lolang on 25/07/24.
+//
+
+import SwiftUI
+
+struct ChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChoiceView()
+}
---
+++ Otterator/View/Component/CustomSegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSegmentedControl.swift
+//  Otterator
+//
+//  Created by Michelle Alvera Lolang on 24/07/24.
+//
+
+import SwiftUI
+
+struct CustomSegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSegmentedControl()
+}
